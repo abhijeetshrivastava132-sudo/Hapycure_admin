3dfdd241d290b093826e7d07db04ca88d394df57
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-06-26 13:30:02 +0545

    Add admin state module

---
+++ js/state.js
@@ -0,0 +1,13 @@
+window.HapyCureAdmin = window.HapyCureAdmin || {};
+
+window.HapyCureAdmin.state = {
+  activePage: "overview",
+  kitchens: [],
+  selectedKitchenId: null,
+  requestSearch: "",
+  message: "",
+  messageType: "success",
+  maintenanceMode: false,
+  orders: [],
+  customers: []
+};
